16ed661316ce006b12ad4fc8d511137df3e19054
htmlexe <96339242+htmlexe@users.noreply.github.com>
2023-10-01 09:29:46 +0700

    Add files via upload

---
+++ home/home/assets/index.js
@@ -0,0 +1,61 @@
+import airbnb from "./airbnb.png";
+import binance from "./binance.png";
+import coinbase from "./coinbase.png";
+import dropbox from "./dropbox.png";
+import logo from "./logo.png";
+import thuc_tien from "./thuc-tien-doanh-nghiep.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import google from "./google.svg";
+import apple from "./apple.svg";
+import arrowUp from "./arrow-up.svg";
+import discount from "./Discount.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import feedback_01 from "./feedback-1.png";
+import feedback_02 from "./feedback-2.png";
+import feedback_03 from "./feedback-3.png";
+import article_01 from "./article-1.png";
+import article_02 from "./article-2.png";
+import article_03 from "./article-3.png";
+import star from "./Star.svg";
+import shield from "./Shield.svg";
+import send from "./Send.svg";
+import logo_partner_01 from "./logo-partner-1.png";
+import logo_partner_02 from "./logo-partner-2.png";
+import logo_partner_03 from "./logo-partner-3.png";
+import logo_partner_04 from "./logo-partner-4.png";
+
+export {
+  airbnb,
+  binance,
+  coinbase,
+  dropbox,
+  logo,
+  thuc_tien,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  feedback_01,
+  feedback_02,
+  feedback_03,
+  article_01,
+  article_02,
+  article_03,
+  logo_partner_01,
+  logo_partner_02,
+  logo_partner_03,
+  logo_partner_04,
+  star,
+  shield, 
+  send
+};
---
+++ home/home/components/index.js
@@ -0,0 +1,17 @@
+import Navbar from "./Navbar";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  CardDeal,
+  Business,
+  Clients,
+  Footer,
+  Testimonials,
+  Hero,
+};
